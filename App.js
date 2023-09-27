5c2e264efcc217070a1f61c9031091f542b9c5a2
Karan Sharma <sharma.karan27oct@gmail.com>
2023-09-27 18:44:58 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+function App(){
+  return (
+    <>
+      <View>
+        <Text style={{fontSize:30}}>Hello React Native i am checking responsiveness</Text>
+      </View>
+    </>
+  );
+}
+
+export default App;
